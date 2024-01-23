ae715d1da62d061cff1bb00be6bfdf6e795a5eef
Milton3000 <kristofferssonmilton@gmail.com>
2024-01-23 15:28:32 +0100

    Added Input from BootStrap, onChange + Array for Episodes

--- src/Pages/Episodes.jsx
+++ src/Pages/Episodes.jsx
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from "react";
+import Cards from "../components/Cards/Cards";
+import InputGroup from "../components/Filters/Category/InputGroup";
 
 const Episodes = () => {
   let [id, setID] = useState(1);
   let [info, setInfo] = useState([]);
+  let [results, setResults] = useState([]);
   let { air_date, name } = info;
   let api = `https://rickandmortyapi.com/api/episode/${id}`;
 
@@ -10,22 +13,42 @@ const Episodes = () => {
     (async function () {
       let data = await fetch(api).then((res) => res.json());
       setInfo(data);
+
+      let mapping = await Promise.all(
+        data.characters.map(async (x) => {
+          const res = await fetch(x);
+          return await res.json();
+        })
+      );
+      setResults(mapping);
     })();
   }, [api]);
-  return <div className="container">
+  return (
+    <div className="container">
+      <div className="row mb-4">
+        <h1 className="text-center mb-4">
+          {" "}
+          Episode :{" "}
+          <span className="text-primary">{name === "" ? "Unknown" : name}</span>
+        </h1>
 
-<h1 className="text-center">
-Episode : {name}
-</h1>
-
-<h5 className="text-center">
-Air Date {air_date}
-</h5>
-
-<div className="row"></div>
-<div className="row"></div>
-
-  </div>;
+        <h5 className="text-center">
+          Air Date {air_date === "" ? "Unknown" : air_date}
+        </h5>
+      </div>
+      <div className="row">
+        <div className="col-3">
+          <h4 className="text-center mb-4">Pick Episodes</h4>
+          <InputGroup setID={setID} name="Episode" total={51} />
+        </div>
+        <div className="col-8">
+          <div className="row">
+            <Cards results={results} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Episodes;
---
+++ src/components/Filters/Category/InputGroup.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const InputGroup = ({ total, name, setID }) => {
+  console.log([...Array(total).keys()]);
+  return (
+    <div class="input-group mb-3">
+      <select
+        onChange={(e) => setID(e.target.value)}
+        class="form-select"
+        id={name}
+      >
+        <option selected> Choose... </option>
+
+        {[...Array(total).keys()].map((x) => {
+          return (
+            <option value={x + 1}>
+              {name} - {x + 1}
+            </option>
+          );
+        })}
+      </select>
+    </div>
+  );
+};
+
+export default InputGroup;
+
+/* Hämtar InputGroup från Boostrap, skapar en ny komponent som heter Input Group
+Använder de-structuring för {total} i Episodes.jsx: <InputGroup total={51}/>
+Gör en map över arrayen och returnar option value x + 1.
+Slutligen, lägger till en onChange på select, för att få sidan att ändra episodes när man trycker på rätt episode.
+*/
