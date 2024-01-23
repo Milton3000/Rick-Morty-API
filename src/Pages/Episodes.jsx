b7479783d4f9d0ffe384b86dc517c74553047d92
Milton3000 <kristofferssonmilton@gmail.com>
2024-01-23 10:57:28 +0100

    Added Episode + API

--- src/Pages/Episodes.jsx
+++ src/Pages/Episodes.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Episodes = () => {
-  return <div>The episodes are here</div>;
+  let [id, setID] = useState(1);
+  let [info, setInfo] = useState([]);
+  let { air_date, name } = info;
+  let api = `https://rickandmortyapi.com/api/episode/${id}`;
+
+  useEffect(() => {
+    (async function () {
+      let data = await fetch(api).then((res) => res.json());
+      setInfo(data);
+    })();
+  }, [api]);
+  return <div className="container">
+
+<h1 className="text-center">
+Episode : {name}
+</h1>
+
+<h5 className="text-center">
+Air Date {air_date}
+</h5>
+
+<div className="row"></div>
+<div className="row"></div>
+
+  </div>;
 };
 
 export default Episodes;
